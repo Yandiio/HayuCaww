10326aa09e7e3292eda5af36b946c6d560db94ac
yandiio <yandifenanda95@gmail.com>
2019-05-17 01:24:40 +0700

    this is first commit

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as Contract;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
+class User extends Authenticatable implements Contract{
+    use Notifiable;
+    use EntrustUserTrait; // add Entrust Package
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name','title', 'first_name', 'last_name','email', 'password','verification','phone',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token','verification',
+    ];
+
+    protected $cast = [
+        'email_verified_at' => 'datetime',
+    ];
+}
